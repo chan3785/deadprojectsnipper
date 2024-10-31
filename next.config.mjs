10e1941f92cdfb9d3f64b6aad0990be457ab7d78
chanho <dirtyface1234@naver.com>
2024-10-31 15:02:22 +0900

    demo ver

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 }
  
 const withMDX = createMDX({
